f812081dcba08d7790285d8f5272eb4fb7c68dc2
phaip88 <230980192+phaip88@users.noreply.github.com>
2026-06-01 05:37:53 -0600

    Update renewal URL in main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ except ImportError:
 # 配置区域
 # ==============================================================================
 RENEW_URLS = [
-    "https://host2play.gratis/server/renew?i=6666666-6666-6666-6666-66666666666",
+    "https://host2play.gratis/server/renew?i=e1e054ad-5b8a-4acf-b987-dbfc6331215b",
     # 添加更多链接
 ]
 
